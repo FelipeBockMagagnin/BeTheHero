561dd52727885d5cb3232654529e9a56015aa2da
Felpi <felipebmagagnin@gmail.com>
2020-03-25 14:15:28 -0300

    Create migrations

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: 'semana omnistack',
-        teste: 'teste'
-    })
+routes.post('/ongs', (request, response) => {    
+    const data = request.body;
+
+    console.log(data);
+
+    return response.json();
 })
 
 
