7eb57ac65ed1e32f08402c71c3245098bda67d45
felipebmagagnin@gmail.com <felipebmagagnin@gmail.com>
2020-03-29 17:53:54 -0300

    Create Frontend in React

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logon from './Pages/Logon';
+import './global.css'
+
+function App() {
+  return (
+    <div>
+      <Logon />
+    </div>
+  );
+}
+
+export default App;
